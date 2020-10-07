fba78fa38e388ffde9f4d116f2f6e9e39df886f7
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2020-10-07 14:33:26 -0400

    null

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+(function ($) {
+  "use strict";
+
+  Drupal.behaviors.equalHeight = {
+    attach: function (context, settings) {
+      $(".equalHeight").matchHeight({
+      });
+    },
+  };
+
+  Drupal.behaviors.fitty = {
+    attach: function (context, settings) {
+      fitty(".fittext");
+    },
+  };
+})(jQuery);
